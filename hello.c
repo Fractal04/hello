a60c3c9eac0fb36f879ba27ce9285a57d455b206
Elijah Milo Halverson <ehalverson3@huskers.unledu>
2023-08-24 09:44:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* Author Elijah Halverson
+    Date 2023/08/24
+    A program that prints my name
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+    printf("My name is Elijah Milo Halverson.\n");
+
+    return 0;
+}
